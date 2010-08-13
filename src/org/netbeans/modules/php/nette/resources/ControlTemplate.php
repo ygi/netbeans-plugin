2d9d806c2badbc0ea538137547f46207e793a6a0
Ondřej Brejla <ondrej@brejla.cz>
2010-08-13 11:13:49 +0200

    New templates were added.

---
+++ src/org/netbeans/modules/php/nette/resources/ControlTemplate.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Description of ${name}
+ *
+ * @author ${user}
+ */
+class ${name} extends BaseControl {
+
+	/**
+	 * (non-phpDoc)
+	 *
+	 * @see Nette\Application\Control#render()
+	 */
+	public function render() {
+		
+	}
+
+}
