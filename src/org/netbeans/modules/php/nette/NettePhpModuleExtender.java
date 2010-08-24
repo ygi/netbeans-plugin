8ee0d4f6f6216763ae4a781e02513cbd4421034d
Ondřej Brejla <ondrej@brejla.cz>
2010-08-24 18:31:28 +0200

    NettePhpModuleExtender: uses resource templates as a pregenerated Nette files and copies Nette files into new project if it is required

--- src/org/netbeans/modules/php/nette/NettePhpModuleExtender.java
+++ src/org/netbeans/modules/php/nette/NettePhpModuleExtender.java
@@ -4,15 +4,15 @@ package org.netbeans.modules.php.nette;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 import javax.swing.JComponent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.modules.php.api.phpmodule.PhpModule;
+import org.netbeans.modules.php.nette.options.NetteOptions;
+import org.netbeans.modules.php.nette.utils.FileUtils;
 import org.netbeans.modules.php.nette.wizards.NewNetteProjectPanel;
 import org.netbeans.modules.php.spi.phpmodule.PhpModuleExtender;
-import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Exceptions;
@@ -20,9 +20,15 @@ import org.openide.util.HelpCtx;
 
 /**
  *
- * @author Radek Ježdík
+ * @author Radek Ježdík, Ondřej Brejla
  */
 public class NettePhpModuleExtender extends PhpModuleExtender {
+
+	private static final String NETTE_LIBS_DIR = "/libs/Nette";
+
+	private static final String NETTE_DOCUMENT_ROOT_DIR = "/document_root";
+
+	private static final String NETTE_APP_DIR = "/app";
 
 	private NewNetteProjectPanel netteProjectPanel;
 
@@ -70,6 +76,11 @@ public class NettePhpModuleExtender extends PhpModuleExtender {
 
             createDocumentRoot(projectDir);
             createApp(projectDir);
+			createLibs(projectDir);
+
+			if (getPanel().isCopyNetteCheckboxSelected()) {
+				copyNetteFiles(projectDir);
+			}
 
             return set;
         } catch (IOException ex) {
@@ -78,8 +89,8 @@ public class NettePhpModuleExtender extends PhpModuleExtender {
         }
     }
 
-    public void createDocumentRoot(String d) throws IOException {
-        File folder = new File(d + "/document_root");
+    private void createDocumentRoot(String d) throws IOException {
+        File folder = new File(d + NETTE_DOCUMENT_ROOT_DIR);
         FileObject doc_root = FileUtil.createFolder(folder);
         set.add(doc_root);
         
@@ -87,57 +98,13 @@ public class NettePhpModuleExtender extends PhpModuleExtender {
         set.add(FileUtil.createFolder(new File(doc_root.getPath() + "/js")));
         set.add(FileUtil.createFolder(new File(doc_root.getPath() + "/images")));
 
-        File f = new File(folder, "index.php");
-        f.createNewFile();
-        FileObject fo = FileUtil.toFileObject(f);
-        if (fo != null) {
-            FileLock lock = fo.lock();
-            PrintWriter w = new PrintWriter(fo.getOutputStream(lock));
-            w.println("<?php");
-            w.println("");
-            w.println("define('WWW_DIR', dirname(__FILE__));");
-            w.println("");
-            w.println("define('APP_DIR', WWW_DIR . '/../app');");
-            w.println("");
-            w.println("define('LIBS_DIR', WWW_DIR . '/../libs');");
-            w.println("");
-            w.println("require APP_DIR . '/bootstrap.php';");
-            w.println("");
-            w.close();
-            lock.releaseLock();
-            //set.add(fo);
-        }
+        FileUtils.copyFile(getClass().getResourceAsStream("/org/netbeans/modules/php/nette/resources/index.php"), new File(d + NETTE_DOCUMENT_ROOT_DIR + "/index.php"));
         
-        f = new File(folder, ".htaccess");
-        f.createNewFile();
-
-        fo = FileUtil.toFileObject(f);
-        if (fo != null) {
-            FileLock lock = fo.lock();
-            PrintWriter w = new PrintWriter(fo.getOutputStream(lock));
-            w.println("# disable directory listing");
-            w.println("Options -Indexes");
-            w.println("");
-            w.println("# mod_rewrite");
-            w.println("<IfModule mod_rewrite.c>");
-                w.println("\tRewriteEngine On");
-                w.println("\tRewriteBase /");
-                w.println("\t");
-                w.println("\t# front controller");
-                w.println("\tRewriteCond %{REQUEST_FILENAME} !-f");
-                w.println("\tRewriteCond %{REQUEST_FILENAME} !-d");
-                w.println("\tRewriteRule !\\.(pdf|js|ico|gif|jpg|png|css|rar|zip|tar\\.gz)$ index.php [L]");
-            w.println("</IfModule>");
-            w.println("");
-            w.close();
-            lock.releaseLock();
-            //set.add(fo);
-        }
+        FileUtils.copyFile(getClass().getResourceAsStream("/org/netbeans/modules/php/nette/resources/.htaccess-DOCUMENT_ROOT"), new File(d + NETTE_DOCUMENT_ROOT_DIR + "/.htaccess"));
     }
 
-
-    public void createApp(String d) throws IOException {
-        File folder = new File(d + "/app");
+    private void createApp(String d) throws IOException {
+        File folder = new File(d + NETTE_APP_DIR);
         FileObject app = FileUtil.createFolder(folder);
         set.add(app);
 
@@ -149,88 +116,22 @@ public class NettePhpModuleExtender extends PhpModuleExtender {
         set.add(FileUtil.createFolder(new File(app.getPath() + "/sessions")));
         set.add(FileUtil.createFolder(new File(app.getPath() + "/logs")));
         
-        File f = new File(folder, "bootstrap.php");
-        f.createNewFile();
-        FileObject fo = FileUtil.toFileObject(f);
-        if (fo != null) {
-            FileLock lock = fo.lock();
-            PrintWriter w = new PrintWriter(fo.getOutputStream(lock));
-            w.println("<?php");
-            w.println("");
-            w.println("require_once LIBS_DIR . '/Nette/loader.php';");
-            w.println("");
-            w.println("Debug::enable();");
-            w.println("");
-            w.println("Environment::loadConfig();");
-            w.println("");
-            w.println("$session = Environment::getSession();");
-            w.println("$session->setSavePath(APP_DIR . '/sessions/');");
-            w.println("");
-            w.println("$application = Environment::getApplication();");
-            w.println("//$application->errorPresenter = 'Error';");
-            w.println("//$application->catchExceptions = TRUE;");
-            w.println("");
-            w.println("$router = $application->getRouter();");
-            w.println("");
-            w.println("$router[] = new Route('index.php', array(");
-                w.println("\t'presenter' => 'Homepage',");
-                w.println("\t'action' => 'default',");
-            w.println("), Route::ONE_WAY);");
-            w.println("");
-            w.println("$router[] = new Route('<presenter>/<action>/<id>', array(");
-                w.println("\t'presenter' => 'Homepage',");
-                w.println("\t'action' => 'default',");
-                w.println("\t'id' => NULL,");
-            w.println("));");
+        FileUtils.copyFile(getClass().getResourceAsStream("/org/netbeans/modules/php/nette/resources/bootstrap.php"), new File(d + NETTE_APP_DIR + "/bootstrap.php"));
 
-            w.println("");
-            w.println("$application->run();");
-            w.close();
-            lock.releaseLock();
-            //set.add(fo);
-        }
+        FileUtils.copyFile(getClass().getResourceAsStream("/org/netbeans/modules/php/nette/resources/.htaccess-APP"), new File(d + NETTE_APP_DIR + "/.htaccess"));
 
-        f = new File(folder, ".htaccess");
-        f.createNewFile();
-        fo = FileUtil.toFileObject(f);
-        if (fo != null) {
-            FileLock lock = fo.lock();
-            PrintWriter w = new PrintWriter(fo.getOutputStream(lock));
-            w.println("Order Allow,Deny");
-            w.println("Deny from all");
-            w.close();
-            lock.releaseLock();
-            //set.add(fo);
-        }
-
-        f = new File(folder, "config.ini");
-        f.createNewFile();
-        fo = FileUtil.toFileObject(f);
-        if (fo != null) {
-            FileLock lock = fo.lock();
-            PrintWriter w = new PrintWriter(fo.getOutputStream(lock));
-            w.println("[common]");
-            w.println("; PHP configuration");
-            w.println("php.date.timezone = \"Europe/Prague\"");
-            w.println("php.iconv.internal_encoding = \"%encoding%\"");
-            w.println("php.mbstring.internal_encoding = \"%encoding%\"");
-            w.println("");
-            w.println("; services");
-            w.println(";service.Nette-Security-IAuthenticator = Model\\Users");
-            w.println("");
-            w.println("service.Nette-Loaders-RobotLoader.option.directory[] = %appDir%");
-            w.println("service.Nette-Loaders-RobotLoader.option.directory[] = %libsDir%");
-            w.println("service.Nette-Loaders-RobotLoader.run = TRUE");
-            w.println("");
-            w.println("[production < common]");
-            w.println("");
-            w.println("");
-            w.println("[development < production]");
-            w.close();
-            lock.releaseLock();
-            set.add(fo);
-        }
+        FileUtils.copyFile(getClass().getResourceAsStream("/org/netbeans/modules/php/nette/resources/config.ini"), new File(d + NETTE_APP_DIR + "/config.ini"));
     }
+
+	private void createLibs(String d) throws IOException {
+		File folder = new File(d + NETTE_LIBS_DIR);
+        FileObject app = FileUtil.createFolder(folder);
+        set.add(app);
+	}
+
+	private void copyNetteFiles(String projectDir) {
+		FileUtils.copyDirectory(new File(NetteOptions.getInstance().getNettePath()), new File(projectDir, NETTE_LIBS_DIR));
+	}
 
 	private NewNetteProjectPanel getPanel() {
 		if (netteProjectPanel == null) {
