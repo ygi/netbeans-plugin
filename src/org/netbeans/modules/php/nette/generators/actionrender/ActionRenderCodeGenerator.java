4be44a44ccff1815e063e3ce1854899696cfa754
Ondřej Brejla <ondrej@brejla.cz>
2010-09-30 19:11:11 +0200

    ActionRenderCodeGenerator: was added

---
+++ src/org/netbeans/modules/php/nette/generators/actionrender/ActionRenderCodeGenerator.java
@@ -0,0 +1,86 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright (c) 2010 Radek Ježdík <redhead@email.cz>, Ondřej Brejla <ondrej@brejla.cz>
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.netbeans.modules.php.nette.generators.actionrender;
+
+import java.util.Collections;
+import java.util.List;
+import javax.swing.text.JTextComponent;
+import org.netbeans.modules.php.nette.wizards.newpresenter.ActionRenderVisualPanel;
+import org.netbeans.spi.editor.codegen.CodeGenerator;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.util.Lookup;
+
+public class ActionRenderCodeGenerator implements CodeGenerator {
+
+	JTextComponent textComp;
+	
+	private ActionRenderVisualPanel panel;
+
+	/**
+	 *
+	 * @param context containing JTextComponent and possibly other items registered by {@link CodeGeneratorContextProvider}
+	 */
+	private ActionRenderCodeGenerator(Lookup context) { // Good practice is not to save Lookup outside ctor
+		textComp = context.lookup(JTextComponent.class);
+	}
+
+	public static class Factory implements CodeGenerator.Factory {
+
+		public List<? extends CodeGenerator> create(Lookup context) {
+			return Collections.singletonList(new ActionRenderCodeGenerator(context));
+		}
+	}
+
+	/**
+	 * The name which will be inserted inside Insert Code dialog
+	 */
+	public String getDisplayName() {
+		return "Add action and/or render method...";
+	}
+
+	/**
+	 * This will be invoked when user chooses this Generator from Insert Code
+	 * dialog
+	 */
+	public void invoke() {
+		processDialog();
+	}
+
+	private boolean processDialog() {
+		panel = new ActionRenderVisualPanel();
+panel.setTemplatesDirectory("/");
+		DialogDescriptor dd = new DialogDescriptor(panel, "Add action and/or render method...", true, DialogDescriptor.OK_CANCEL_OPTION, DialogDescriptor.OK_OPTION, null);
+
+		Object result = DialogDisplayer.getDefault().notify(dd);
+
+		if (result != NotifyDescriptor.OK_OPTION) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
