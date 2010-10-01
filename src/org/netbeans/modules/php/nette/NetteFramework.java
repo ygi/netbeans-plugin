a85f401a11123b5263a09e8001be1425a24340a6
Ondřej Brejla <ondrej@brejla.cz>
2010-10-01 17:42:56 +0200

    Nette Framework constants.

---
+++ src/org/netbeans/modules/php/nette/NetteFramework.java
@@ -0,0 +1,55 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright (c) 2010 Radek Ježdík <redhead@email.cz>, Ondřej Brejla <ondrej@brejla.cz>
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.netbeans.modules.php.nette;
+
+
+/**
+ * NetteFramework static constants.
+ *
+ * @author Ondřej Brejla <ondrej@brejla.cz>
+ */
+public final class NetteFramework {
+
+	public static final String NETTE_APP_DIR = "/app";
+
+	public static final String NETTE_DOCUMENT_ROOT_DIR = "/document_root";
+
+	public static final String NETTE_LATTE_TEMPLATE_EXTENSION = ".phtml";
+
+	public static final String NETTE_LIBS_DIR = "/libs/Nette";
+
+	public static final String NETTE_LOG_DIR = "/log";
+
+	public static final String NETTE_PRESENTER_EXTENSION = ".php";
+	
+	public static final String NETTE_TEMP_DIR = "/temp";
+
+	public static final String NETTE_ACTION_METHOD_PREFIX = "action";
+
+	public static final String NETTE_RENDER_METHOD_PREFIX = "render";
+
+	private NetteFramework() {
+	}
+
+}
