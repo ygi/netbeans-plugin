5fdf7f2e3d4f9625b98e94926bf6b30cca59f5dc
redhead <redhead@email.cz>
2010-10-28 01:58:11 +0200

    class name fix

--- src/org/netbeans/modules/php/nette/editor/Embedder.java
+++ src/org/netbeans/modules/php/nette/editor/Embedder.java
@@ -32,7 +32,7 @@ import java.util.Collections;
 import java.util.List;
 import org.netbeans.modules.parsing.api.Embedding;
 import org.netbeans.modules.parsing.api.Snapshot;
-import org.netbeans.modules.php.nette.utils.MacroSyntaxUtils;
+import org.netbeans.modules.php.nette.utils.SyntaxUtils;
 
 /**
  *
@@ -50,13 +50,13 @@ abstract public class Embedder {
 	}
 
 	public boolean embed(String text) {
-		text = MacroSyntaxUtils.replaceCommonSyntax(text);
+		text = SyntaxUtils.replaceCommonSyntax(text);
 		return embeddings.add(snapshot.create(text, getMimeType()));
 	}
 
 	public boolean embed(int start, int length) {
 		String text = snapshot.getText().subSequence(start, start+length).toString();
-		text = MacroSyntaxUtils.replaceCommonSyntax(text);
+		text = SyntaxUtils.replaceCommonSyntax(text);
 		return embeddings.add(snapshot.create(text, getMimeType()));
 	}
 
